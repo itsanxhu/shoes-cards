78828509683769ef64947f42de4907343cd5c4f2
anshu <anshhufx@gmail.com>
2026-05-27 19:22:38 +0530

    add key prop to shoe cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,49 @@ import { DotBackgroundDemo } from "./Components/Dotbg";
 import ShoesCard from "./Components/ShoesCard";
 
 const App = () => {
+  const shoes = [
+    {
+      id: 1,
+      title: "Jordan Sixty Plus Low",
+      brand: "Nike",
+      img: "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/c/e/ce80759Nike-IH2047-100_1.jpg?rnd=20200526195200&tr=w-1080",
+      hoverImg:
+        "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/c/e/ce80759Nike-IH2047-100_8.jpg?rnd=20200526195200&tr=w-1080",
+      price: 138,
+    },
+    {
+      id: 2,
+      title: "Nike Structure Plus",
+      brand: "Nike",
+      img: "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/b/ab828aaNike-HQ3048-400_1.jpg?rnd=20200526195200&tr=w-1080",
+      hoverImg:
+        "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/b/ab828aaNike-HQ3048-400_8.jpg?rnd=20200526195200&tr=w-1080",
+      price: 157,
+    },
+    {
+      id: 3,
+      title: "Nike Air Max Alpha Trainer 6",
+      brand: "Nike",
+      img: "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/b/ab828aaNike-FQ1833-014_1.jpg?rnd=20200526195200&tr=w-1080",
+      hoverImg:
+        "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/b/ab828aaNike-FQ1833-014_8.jpg?rnd=20200526195200&tr=w-1080",
+      price: 124,
+    },
+  ];
   return (
     <div className="relative min-h-screen overflow-hidden">
       <DotBackgroundDemo />
       <div className="relative z-10 flex flex-col lg:flex-row justify-center gap-10">
-        <ShoesCard
-          title="Jordan Sixty Plus Low"
-          brand="Nike"
-          img="https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/c/e/ce80759Nike-IH2047-100_1.jpg?rnd=20200526195200&tr=w-1080"
-          hoverImg="https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/c/e/ce80759Nike-IH2047-100_8.jpg?rnd=20200526195200&tr=w-1080"
-          price={138}
-        />
-        <ShoesCard
-          title="Nike Structure Plus"
-          brand="Nike"
-          img="https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/b/ab828aaNike-HQ3048-400_1.jpg?rnd=20200526195200&tr=w-1080"
-          hoverImg="https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/b/ab828aaNike-HQ3048-400_8.jpg?rnd=20200526195200&tr=w-1080"
-          price={157}
-        />
-        <ShoesCard
-          title="Nike Air Max Alpha Trainer 6"
-          brand="Nike"
-          img="https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/b/ab828aaNike-FQ1833-014_1.jpg?rnd=20200526195200&tr=w-1080"
-          hoverImg="https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/b/ab828aaNike-FQ1833-014_8.jpg?rnd=20200526195200&tr=w-1080"
-          price={124}
-        />
+        {shoes.map((shoe) => (
+          <ShoesCard
+            key={shoe.id}
+            title={shoe.title}
+            brand={shoe.brand}
+            img={shoe.img}
+            hoverImg={shoe.hoverImg}
+            price={shoe.price}
+          />
+        ))}
       </div>
     </div>
   );
